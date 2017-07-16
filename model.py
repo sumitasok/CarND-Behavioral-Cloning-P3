dafe8fb79a0ac28f4b0ee32e2441a8061ee4c042
Sumit Asok <sumitasok@gmail.com>
2017-07-16 18:54:16 +0530

    model.add lambda x/255.0 epochs 7 969/969 [==============================] - 1s - loss: 960.1148 - val_loss: 20.6427 Epoch 2/7 969/969 [==============================] - 1s - loss: 38.8750 - val_loss: 0.6385 Epoch 3/7 969/969 [==============================] - 1s - loss: 2.2357 - val_loss: 1.3249 Epoch 4/7 969/969 [==============================] - 1s - loss: 0.3604 - val_loss: 0.4223 Epoch 5/7 969/969 [==============================] - 1s - loss: 0.2306 - val_loss: 0.4414 Epoch 6/7 969/969 [==============================] - 1s - loss: 0.2026 - val_loss: 0.2474 Epoch 7/7 969/969 [==============================] - 1s - loss: 0.1843 - val_loss: 0.2568

--- model.py
+++ model.py
@@ -26,13 +26,14 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
-model.add(Flatten(input_shape = (160, 320, 3)))
+model.add(Lambda(lambda x: x /255.0, input_shape=(160, 320, 3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=9)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
 
 model.save('model.h5')
