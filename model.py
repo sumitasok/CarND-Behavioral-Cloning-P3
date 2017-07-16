499e19ba1b30c07712ce452ffa174ff9060bdc60
Sumit Asok <sumitasok@gmail.com>
2017-07-16 18:41:50 +0530

    added model taught in udacity behavioral cloning training lecture

---
+++ model.py
@@ -0,0 +1,38 @@
+import numpy as np
+import csv
+import cv2
+
+base_path =  '/Users/sumitasok/Documents/Self-Driving Car/Behavioural Cloning/Training Data/'
+
+lines = []
+with open(base_path + 'driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = base_path + '/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape = (160, 320, 3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=9)
+
+model.save('model.h5')
