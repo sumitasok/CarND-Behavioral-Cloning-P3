9ed1166018bae050585e72338ba8c74f0ef0c840
Sumit Asok <sumitasok@gmail.com>
2017-07-29 14:48:13 +0530

    traning with udacity provided data
    good improvement in training and validation accuracy
    
    6428/6428 [==============================] - 18s - loss: 0.0215 - val_loss: 0.0122
    Epoch 2/7
    6428/6428 [==============================] - 17s - loss: 0.0136 - val_loss: 0.0143
    Epoch 3/7
    6428/6428 [==============================] - 17s - loss: 0.0128 - val_loss: 0.0130
    Epoch 4/7
    6428/6428 [==============================] - 17s - loss: 0.0124 - val_loss: 0.0116
    Epoch 5/7
    6428/6428 [==============================] - 17s - loss: 0.0117 - val_loss: 0.0116
    Epoch 6/7
    6428/6428 [==============================] - 17s - loss: 0.0116 - val_loss: 0.0113
    Epoch 7/7
    6428/6428 [==============================] - 17s - loss: 0.0113 - val_loss: 0.0118

--- model.py
+++ model.py
@@ -3,13 +3,13 @@ import numpy as np
 import csv
 import cv2
 
-base_path =  '/Users/sumitasok/Documents/Self-Driving Car/Behavioural Cloning/Training Data/'
+base_path =  '/Users/sumitasok/Documents/Self-Driving Car/Behavioural Cloning/data/'
 
 parser = argparse.ArgumentParser(description='Remote Driving')
 parser.add_argument(
     'training_data_base_path',
     type=str,
-    help='Path to training data, this dir should have /IMG and driving_log.csv'
+    help='Path to training data, this dir should have /IMG and /driving_log.csv'
 )
 
 lines = []
@@ -25,7 +25,7 @@ measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = base_path + '/IMG/' + filename
+    current_path = base_path + 'IMG/' + filename
     image = cv2.imread(current_path)
     image_resized = cv2.resize(image[60:140,:], (64,64))
     images.append(image_resized)
