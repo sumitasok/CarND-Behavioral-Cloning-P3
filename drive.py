b1bef9c69ecf5585fe41805ea554a3d4d1914a47
Sumit Asok <sumitasok@gmail.com>
2017-07-29 20:24:30 +0530

    Running 3 epochs using generator in previous run, the image was processed outside the eras, which means only training and validation data was processed. which means the input to model was (64, 64, 3) but the actual data coming from the autonomous driving was un cropped (160, 80, 3). This could have been the cause the autonomous run did not work. now moved the processing to generator.
    6428/6428 [==============================] - 519s - loss: 0.0101 - val_loss: 0.0106
    Epoch 2/3
    6428/6428 [==============================] - 513s - loss: 0.0089 - val_loss: 0.0107
    Epoch 3/3
    6428/6428 [==============================] - 508s - loss: 0.0085 - val_loss: 0.0106

--- drive.py
+++ drive.py
@@ -61,6 +61,7 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        image_array = cv2.resize(image_array[60:140,:],(64,64))###my modification
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
--- model.py
+++ model.py
@@ -2,8 +2,33 @@ import argparse
 import numpy as np
 import csv
 import cv2
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = base_path+ 'IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                image_resized = cv2.resize(center_image[60:140,:], (64,64))
+                center_angle = float(batch_sample[3])
+                images.append(image_resized)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield (X_train, y_train)
 
 base_path =  '/Users/sumitasok/Documents/Self-Driving Car/Behavioural Cloning/data/'
+# base_path =  '/Users/sumitasok/Documents/Self-Driving Car/Behavioural Cloning/Training data/'
 
 parser = argparse.ArgumentParser(description='Remote Driving')
 parser.add_argument(
@@ -22,18 +47,27 @@ with open(training_data_base_path + 'driving_log.csv') as csvfile:
 images = []
 measurements = []
 
-for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = base_path + 'IMG/' + filename
-    image = cv2.imread(current_path)
-    image_resized = cv2.resize(image[60:140,:], (64,64))
-    images.append(image_resized)
-    measurement = float(line[3])
-    measurements.append(measurement)
+samples = []
+with open(training_data_base_path + 'driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+
+# for line in lines:
+#     source_path = line[0]
+#     filename = source_path.split('/')[-1]
+#     current_path = base_path + 'IMG/' + filename
+#     image = cv2.imread(current_path)
+#     image_resized = cv2.resize(image[60:140,:], (64,64))
+#     images.append(image_resized)
+#     measurement = float(line[3])
+#     measurements.append(measurement)
+
+# X_train = np.array(images)
+# y_train = np.array(measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Dropout
@@ -41,9 +75,12 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 import keras.regularizers as regularizers
 
+train_generator = generator(train_samples, 32)
+validation_generator = generator(validation_samples, 32)
+
 model = Sequential()
 # normalisation of training data.
-model.add(Lambda(lambda x: ((x/255.0)-0.5), input_shape=(64, 64, 3)))
+model.add(Lambda(lambda x: ((x/255.0)-0.5), input_shape=(64, 64, 3), output_shape=(64, 64, 3)))
 model.add(Convolution2D(3,3,3, activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(3,3,3, activation="relu"))
@@ -58,6 +95,11 @@ model.add(Dropout(0.5))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
+# model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
 
-model.save('model.h5')
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("model.h5")
